677126f8cab02492483068df15afcbff048b0fca
Denisanyakin <51450175+Denisanyakin@users.noreply.github.com>
2019-06-24 17:21:46 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,34 @@
 package ru.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("wold");
+    hello("user");
+    hello("Denys");
 
-    System.out.println("Hello, world!");
+    double l = 5;
+    System.out.println("Ploshad kvadrata so storonoj " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Ploshad pryamougolnika so storonami " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
